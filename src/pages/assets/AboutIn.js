75d2dd8a83e598e103c367aa3b7c00d338d81c17
AnglesElsa <silverwisdom615@gmail.com>
2023-06-13 17:34:32 +0000

    complete portfolio

---
+++ src/pages/assets/AboutIn.js
@@ -0,0 +1,47 @@
+const personal_info_data = [
+  {
+    index: 1,
+    section_one: [
+      {
+        index: 1,
+        data_type: "Age",
+        data_value: "26",
+      },
+      {
+        index: 2,
+        data_type: "Gender",
+        data_value: "female",
+      },
+      {
+        index: 3,
+        data_type: "Languages",
+        data_value: "English",
+      },
+    ],
+    section_two: [
+      {
+        index: 1,
+        data_type: "Skype",
+        data_value: "live:.cid.ee9c35e705c7b869",
+        icon_class: "fa-solid fa-phone-flip",
+        ic_color: "#e64a19",
+      },
+      {
+        index: 2,
+        data_type: "Email",
+        data_value: "silverwisdom615@gmail.com",
+        icon_class: "fa-solid fa-envelope",
+        ic_color: "#00838F",
+      },
+      {
+        index: 3,
+        data_type: "Address",
+        data_value: "Montreal, Canada - H9R 3J4",
+        icon_class: "fa-sharp fa-solid fa-location-dot",
+        ic_color: "rgb(34 119 0)",
+      },
+    ],
+  },
+];
+
+export default personal_info_data;
